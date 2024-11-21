655e4d8a39c75da5703929fda2e12a5ed0603b0c
AudreyW05 <78786071+AudreyW05@users.noreply.github.com>
2024-11-20 16:55:51 -0800

    give permission to local host

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-// src/main.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
@@ -7,6 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for specific domains
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // if you are using cookies or sessions
+  });
+
   app.useGlobalFilters(new PrismaExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +21,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
